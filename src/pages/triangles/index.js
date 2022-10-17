2822023a59cfec919ecfca8103fb055ee7ec901f
Aman Pawar <aman.pawar@toppr.com>
2022-10-17 15:36:46 +0530

    added react full page library and corresponding changes

--- src/pages/triangles/index.js
+++ src/pages/triangles/index.js
@@ -1,12 +1,31 @@
 import Slide1 from "./Slide1";
 import Slide2 from "./Slide2";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 export const Triangles = () => {
   return (
-    <div>
-      Triangles concept page
-      <Slide1 />
-      <Slide2 />
-    </div>
+    // <div>
+    //   Triangles concept page
+    //   <Slide1 />
+    //   <Slide2 />
+    // </div>
+    <ReactFullpage
+      //fullpage options
+      // licenseKey={"YOUR_KEY_HERE"}
+      scrollingSpeed={1000} /* Options here */
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <Slide1 />
+              <button onClick={() => fullpageApi.moveSectionDown()}>Click me to move down</button>
+            </div>
+            <div className="section">
+              <Slide2 />
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
   );
 };
