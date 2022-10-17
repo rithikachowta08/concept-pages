d584eb63f0194d6085f0bc679489cf3e266c989c
Aman Pawar <aman.pawar@toppr.com>
2022-10-17 19:30:34 +0530

    changes for full page scroll and added next slide button

--- src/pages/triangles/index.js
+++ src/pages/triangles/index.js
@@ -2,39 +2,52 @@ import styled from "styled-components/macro";
 import Slide1 from "./Slide1";
 import Slide2 from "./Slide2";
 import ReactFullpage from "@fullpage/react-fullpage";
+import DownArrowIcon from "assets/DownArrowIcon";
 
 const PageWrap = styled.div`
-  height: 100%;
-  width: 100%;
+  height: 100vh;
+  width: 100vw;
 `;
 
 export const Triangles = () => {
+  const SlideArray = [
+    <Slide1 key={0} />,
+    <Slide1 key={1} />,
+    <Slide1 key={2} />,
+    <Slide2 key={3} />
+  ];
+
   return (
-    // <div>
-    //   Triangles concept page
-    //   <Slide1 />
-    //   <Slide2 />
-    // </div>
-    // <ReactFullpage
-    //   //fullpage options
-    //   // licenseKey={"YOUR_KEY_HERE"}
-    //   scrollingSpeed={1000} /* Options here */
-    //   render={({ state, fullpageApi }) => {
-    //     return (
-    //       <ReactFullpage.Wrapper>
-    //         <div className="section">
-    //           <Slide1 />
-    //           <button onClick={() => fullpageApi.moveSectionDown()}>Click me to move down</button>
-    //         </div>
-    //         <div className="section">
-    //           <Slide2 />
-    //         </div>
-    //       </ReactFullpage.Wrapper>
-    //     );
-    //   }}
-    // />
-    <PageWrap>
-      <Slide1 />
-    </PageWrap>
+    <ReactFullpage
+      //fullpage options
+      // licenseKey={"YOUR_KEY_HERE"}
+      navigation
+      navigationTooltips={["Slide1", "Slide2", "Slide3", "Slide4"]}
+      navigationPosition={"left"}
+      scrollingSpeed={1000} /* Options here */
+      render={({ state, fullpageApi }) => {
+        return (
+          <>
+            <ReactFullpage.Wrapper>
+              {SlideArray.map((itm, idx) => (
+                <div
+                  className="section"
+                  key={idx}>
+                  <PageWrap>{itm}</PageWrap>
+                  <div
+                    onClick={() => fullpageApi.moveSectionDown()}
+                    style={{ cursor: "pointer" }}>
+                    <DownArrowIcon
+                      color={idx ? "#333333" : "#ffffff"}
+                      className="down-arrow-icon"
+                    />
+                  </div>
+                </div>
+              ))}
+            </ReactFullpage.Wrapper>
+          </>
+        );
+      }}
+    />
   );
 };
