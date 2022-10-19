4eb2d8aeeea6ddc4ac99ac8983a37aa33b251447
aman-pawar <69139656+aman-pawar@users.noreply.github.com>
2022-10-19 15:55:54 +0530

    Add full page scrolling support
    * added react full page library and corresponding changes
    
    * changes for full page scroll and added next slide button
    
    * custom wrapper component for full page scroll added
    
    * navigation button changes

--- src/pages/triangles/index.js
+++ src/pages/triangles/index.js
@@ -1,14 +1,18 @@
 import { PageWrap } from "components/StyledElements";
 import Slide1 from "./Slide1";
 import Slide2 from "./Slide2";
+import { FullPageCustomWrapper } from "components/FullPageCustomWrapper";
 import Slide3 from "./Slide3";
 
 export const Triangles = () => {
+  const SlideArray = [<Slide1 key={0} />, <Slide2 key={1} />, <Slide3 key={2} />];
+
+  const SlideNameList = ["Slide1", "Slide2", "Slide3"];
+
   return (
-    <PageWrap>
-      <Slide1 />
-      <Slide2 />
-      <Slide3 />
-    </PageWrap>
+    <FullPageCustomWrapper
+      slidesComponentList={SlideArray}
+      slidesNamesList={SlideNameList}
+    />
   );
 };
