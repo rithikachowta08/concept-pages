4be3f90a438b0119aeb1e8c30cece1fd1640fa36
Jeeva <jeeva@Jeevas-MacBook-Pro.local>
2022-12-01 17:28:15 +0530

    changes

--- src/components/text/index.js
+++
@@ -1,6 +0,0 @@
-import ContentList from "./ContentList";
-import Paragraph from "./Paragraph";
-import TextSpan from "./TextSpan";
-import Title from "./Title";
-
-export { ContentList, Paragraph, TextSpan, Title };
