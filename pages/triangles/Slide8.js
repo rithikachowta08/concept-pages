fae49f7771857b2691176af5a184e95307495fd2
Jeeva <jeeva@Jeevas-MacBook-Pro.local>
2022-12-02 13:36:26 +0530

    changes

--- pages/triangles/Slide8.js
+++ pages/triangles/Slide8.js
@@ -28,19 +28,11 @@ const Slide8 = () => {
   const triggerToast = (isRightAngled) => {
     if (isRightAngled) {
       toast.success("That is correct! ABC is a right angled triangle", {
-        icon: (
-          <Icon
-            src={success}
-            width="20px"></Icon>
-        )
+        icon: <Icon src={success} width="20px"></Icon>,
       });
     } else {
       toast.error("A right angled triangle has one angle equal to 90°", {
-        icon: (
-          <Icon
-            src={error}
-            width="20px"></Icon>
-        )
+        icon: <Icon src={error} width="20px"></Icon>,
       });
     }
   };
@@ -53,18 +45,12 @@ const Slide8 = () => {
   }, []);
 
   return (
-    <SlideWrap
-      bg={bg}
-      padding="100px">
-      <Flex
-        alignItems="center"
-        justifyContent="space-around">
-        <Paragraph maxWidth="30%">Move the vertices to form a right angled triangle</Paragraph>
-        <IFrame
-          src="./triangle.html"
-          allowFullScreen
-          frameBorder="0"
-        />
+    <SlideWrap bg={bg} padding="100px">
+      <Flex alignItems="center" justifyContent="space-around">
+        <Paragraph maxWidth="30%">
+          Move the vertices to form a right angled triangle
+        </Paragraph>
+        <IFrame src="./triangle.html" allowFullScreen frameBorder="0" />
       </Flex>
     </SlideWrap>
   );
