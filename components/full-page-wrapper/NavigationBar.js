1ad69de31f23c2068daf7ec60be1482866517a79
Aman Pawar <aman.pawar@toppr.com>
2022-12-02 16:43:19 +0530

    config changes

--- components/full-page-wrapper/NavigationBar.js
+++ components/full-page-wrapper/NavigationBar.js
@@ -4,6 +4,9 @@ import { Flex, Icon } from "../StyledElements";
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import { colors } from "../../utils/colors";
+// import dynamic from 'next/dynamic'
+
+// const left_caret = dynamic(() => import('public/assets/left_caret.svg'))
 const left_caret = "assets/left_caret.svg";
 
 const NavWrap = styled.div`
