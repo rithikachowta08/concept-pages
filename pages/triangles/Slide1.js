be60a7ddf3c1a6ee895297662eb8c85d40be6e85
Jeeva <jeeva@Jeevas-MacBook-Pro.local>
2022-12-05 11:15:18 +0530

    changes for the toast

--- pages/triangles/Slide1.js
+++ pages/triangles/Slide1.js
@@ -1,16 +1,17 @@
 import { PropTypes } from "prop-types";
 import { SlideWrap } from "../../components/StyledElements";
 import { Title, ContentList } from "../../components/text";
-// import bg from "../../public/assets/purple_bg_triangles.png";
 
 const Slide1 = ({ moveToSection }) => {
   return (
-    <SlideWrap
-      bg={"/assets/purple_bg_triangles.png"}
-      paddingLeft="200px">
+    <SlideWrap bg={"/assets/purple_bg_triangles.png"} paddingLeft="200px">
       <Title marginBottom="50px">Triangles</Title>
       <ContentList
-        items={["What is a triangle?", "Types of triangles", "Properties of triangles"]}
+        items={[
+          "What is a triangle?",
+          "Types of triangles",
+          "Properties of triangles",
+        ]}
         moveTo={moveToSection}
         anchorIdxes={[2, 4, 10]}
       />
@@ -19,7 +20,7 @@ const Slide1 = ({ moveToSection }) => {
 };
 
 Slide1.propTypes = {
-  moveToSection: PropTypes.any
+  moveToSection: PropTypes.any,
 };
 
 export default Slide1;
