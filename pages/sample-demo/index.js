77fdf8c9e8b2ac843095c708e5aa9f58f16565cd
Rithika Chowta <rithika.chowta08@gmail.com>
2022-12-23 17:14:11 +0530

    Add sample page 2

--- pages/sample-demo/index.js
+++ pages/sample-demo/index.js
@@ -11,9 +11,7 @@ const VolumeOfCube = ({}) => {
       <Slide1 key={0} />,
       <Slide2 key={1} />,
       <Slide3 key={2} />,
-      <Slide4 key={3} />,
-      <Slide5 key={4} />,
-      <Slide6 key={5} />,
+      <Slide6 key={3} />,
    ];
 
    const navigationSections = [
