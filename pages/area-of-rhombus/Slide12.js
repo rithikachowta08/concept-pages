1f87c84f9af5790b7c77464102a8f4f22a967077
jeeva-thangavel <95739160+jeeva-thangavel@users.noreply.github.com>
2022-12-23 11:31:28 +0530

    Pages/area of rhombus (#43)
    * EquationTable component added to render equations
    
    * changes for area of rhombus
    
    * Add highlighting logic to katex, fix textSpan styling
    
    * changes for rhombus
    
    * Remove margin
    
    * changes for adding equations
    
    * adding lazy loading to the files
    
    * changing on hover function to event handling
    
    * adding assets
    
    * adding assets
    
    * adding assets
    
    * adding assets
    
    * adding assets
    
    * pull dev_copy
    
    * changes for other pages
    
    * changes for other pages
    
    Co-authored-by: Pushpak Gupta <pushpak.gupta@infobeans.com>
    Co-authored-by: Rithika Chowta <rithika.chowta08@gmail.com>

---
+++ pages/area-of-rhombus/Slide12.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { colors } from "utils/colors";
+import dynamic from "next/dynamic";
+const TransitionImage = dynamic(() =>
+  import("components/media/TransitionImage")
+);
+const TextAndDiagramSlide = dynamic(() =>
+  import("components/slides/TextAndDiagramSlide")
+);
+const Pill = dynamic(() => import("components/Pill"));
+const Paragraph = dynamic(() =>
+  import("components/text").then((mod) => mod.Paragraph)
+);
+const TextSpanBg = dynamic(() =>
+  import("components/text").then((mod) => mod.TextSpanBg)
+);
+const MathElement = dynamic(() => import("components/MathElement/index.js"));
+const rhombus_full = "assets/area-of-rhombus/slide12/1.svg";
+const rhombus_lb = "assets/area-of-rhombus/slide12/2.svg";
+
+const Slide12 = ({ downIcon }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onHover = (e) => {
+    setActiveIndex(Number(e.target.id));
+  };
+  const onHoverOut = (e) => {
+    setActiveIndex(0);
+  };
+
+  let latex = [];
+  latex.push(`= a^2 \\sin \\theta`);
+  let mathjaxCounter = 0;
+  return (
+    <TextAndDiagramSlide
+      diagram={
+        <TransitionImage
+          images={[rhombus_full, rhombus_lb]}
+          altTexts={[
+            "Diagram of rhombus with side length ‘a’ and included angle shown",
+            "Diagram of rhombus with side length ‘a’ and included angle highlighted",
+          ]}
+          activeIndex={activeIndex}
+        />
+      }
+      downIcon={downIcon}
+      bg="DARK"
+      secondaryTitle="Area of a Rhombus"
+      title="Using Trigonometry"
+    >
+      <Paragraph color={colors.WHITE}>
+        In a rhombus, if the lengths of{" "}
+        <TextSpanBg
+          id={1}
+          onHover={onHover}
+          onHoverOut={onHoverOut}
+          hoverColor={colors.AQUA}
+          color={colors.RED}
+        >
+          two adjacent sides and the included angle
+        </TextSpanBg>{" "}
+        between them are given, then the area can be calculated as:
+      </Paragraph>
+      <Pill>
+        Area of a rhombus <MathElement htmlString={latex[mathjaxCounter++]} />
+      </Pill>
+    </TextAndDiagramSlide>
+  );
+};
+
+export default Slide12;
---
+++ pages/area-of-rhombus/Slide2.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { colors } from "utils/colors";
+import dynamic from "next/dynamic";
+const TransitionImage = dynamic(() =>
+  import("components/media/TransitionImage")
+);
+const TextAndDiagramSlide = dynamic(() =>
+  import("components/slides/TextAndDiagramSlide")
+);
+const Paragraph = dynamic(() =>
+  import("components/text").then((mod) => mod.Paragraph)
+);
+const TextSpanBg = dynamic(() =>
+  import("components/text").then((mod) => mod.TextSpanBg)
+);
+
+const rhombus_full = "assets/area-of-rhombus/slide2/1.svg";
+const rhombus_arrow = "assets/area-of-rhombus/slide2/2.svg";
+const rhombus_line = "assets/area-of-rhombus/slide2/3.svg";
+import { lineHeightProp } from "utils/fontStyles";
+
+const Slide2 = ({ downIcon }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onHover = (e) => {
+    setActiveIndex(Number(e.target.id));
+  };
+  const onHoverOut = (e) => {
+    setActiveIndex(0);
+  };
+  return (
+    <TextAndDiagramSlide
+      title="What is a Rhombus?"
+      diagram={
+        <TransitionImage
+          images={[rhombus_full, rhombus_arrow, rhombus_line]}
+          altTexts={[
+            "Diagram of rhombus",
+            "Diagram of rhombus with parallel sides highlighted",
+            "Diagram of rhombus with equal sides highlighted",
+          ]}
+          activeIndex={activeIndex}
+        />
+      }
+      downIcon={downIcon}
+    >
+      <Paragraph lineHeight={lineHeightProp}>
+        A rhombus is a quadrilateral whose both pairs of opposite sides are
+        &nbsp;
+        <TextSpanBg
+          id={1}
+          onHover={onHover}
+          onHoverOut={onHoverOut}
+          color={colors.RED}
+          hoverColor={colors.DARK_BLUE}
+        >
+          parallel
+        </TextSpanBg>
+        &nbsp;and all sides are&nbsp;
+        <TextSpanBg
+          id={2}
+          onHover={onHover}
+          onHoverOut={onHoverOut}
+          color={colors.RED}
+          hoverColor={colors.DARK_BLUE}
+        >
+          equal
+        </TextSpanBg>
+        .
+      </Paragraph>
+    </TextAndDiagramSlide>
+  );
+};
+
+export default Slide2;
