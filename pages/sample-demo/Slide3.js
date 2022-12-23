d54c04a54031dd06341c34c90e6289e3466916fe
Rithika Chowta <rithika.chowta08@gmail.com>
2022-12-23 17:03:50 +0530

    Add sample page

---
+++ pages/sample-demo/Slide3.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { Flex, StyledImg } from "components/StyledElements";
+import { Paragraph, TextSpanBg, ModalTriggerText } from "components/text";
+import TransitionImage from "components/media/TransitionImage";
+import TextAndDiagramSlide from "components/slides/TextAndDiagramSlide";
+import Modal from "components/layout/Modal";
+const cube_red = "assets/volume-of-cube/slide3/1.svg";
+const cube_spaces = "assets/volume-of-cube/slide3/2.svg";
+const cube_unit = "assets/volume-of-cube/slide3/3.svg";
+import { colors } from "utils/colors";
+import { lineHeightProp } from "utils/fontStyles";
+
+const Slide3 = ({ downIcon }) => {
+   const [activeIndex, setActiveIndex] = useState(0);
+   const [isModalOpen, setIsModalOpen] = useState(false);
+   const modalContent = (
+      <Flex direction="column">
+         <Paragraph color="white" marginBottom="1vh" fontSize="1.5rem">
+            Unit cube is a cube whose sides are 1 unit long
+         </Paragraph>
+         <StyledImg
+            src={cube_unit}
+            alignSelf="center"
+            width="400px"
+            marginBottom="50px"
+         />
+      </Flex>
+   );
+   const onClick = () => {
+      setIsModalOpen(!isModalOpen);
+   };
+   const onDismiss = () => {
+      setIsModalOpen(false);
+   };
+   const onHover = (e) => {
+      setActiveIndex(Number(e.target.id));
+   };
+
+   const onHoverOut = (e) => {
+      setActiveIndex(0);
+   };
+   return (
+      <TextAndDiagramSlide
+         downIcon={downIcon}
+         title="What is the volume of a cube?"
+         diagram={
+            <TransitionImage
+               images={[cube_red, cube_spaces]}
+               activeIndex={activeIndex}
+            />
+         }
+      >
+         <Modal
+            isOpen={isModalOpen}
+            title="Unit Cube"
+            content={modalContent}
+            onDismiss={onDismiss}
+         />
+         <Paragraph lineHeight={lineHeightProp}>
+            The volume of a cube is the{" "}
+            <TextSpanBg
+               id={1}
+               onHover={onHover}
+               onHoverOut={onHoverOut}
+               color={colors.RED}
+               hoverColor={colors.DARK_BLUE}
+            >
+               space
+            </TextSpanBg>{" "}
+            occupied by it. It is also the total number of{" "}
+            <ModalTriggerText id={2} onClick={onClick}>
+               unit cubes
+            </ModalTriggerText>{" "}
+            that the cube can accomodate.
+         </Paragraph>
+      </TextAndDiagramSlide>
+   );
+};
+
+export default Slide3;
