5c6882a94fb21197ed0f4d5379947ad34bdb0325
Rithika Chowta <rithika.chowta08@gmail.com>
2022-12-24 18:29:52 +0530

    Revert "Simplify demo"
    This reverts commit d258ea3d1c175e443323e6473e83f2b6830adade.

--- pages/sample-demo/index.js
+++ pages/sample-demo/index.js
@@ -1,8 +1,18 @@
 import { FullPageCustomWrapper } from "components/full-page-wrapper/FullPageCustomWrapper.js";
+import Slide1 from "./Slide1";
 import Slide2 from "./Slide2";
+import Slide3 from "./Slide3";
+import Slide4 from "./Slide4";
+import Slide5 from "./Slide5";
+import Slide6 from "./Slide6";
 
 const VolumeOfCube = ({}) => {
-   const SlideArray = [<Slide2 key={1} />];
+   const SlideArray = [
+      <Slide1 key={0} />,
+      <Slide2 key={1} />,
+      <Slide3 key={2} />,
+      <Slide6 key={3} />,
+   ];
 
    const navigationSections = [
       { title: "What is the volume of a cube?", slides: [1, 2, 3, 4] },
