47f1e0fe0e4e466946440568ff2213ede903320b
jeeva.thangavel <jeeva.thangavel@byjus.com>
2022-12-26 12:23:52 +0530

    changes

--- pages/surface-area-of-cone/Slide10.js
+++ pages/surface-area-of-cone/Slide10.js
@@ -13,11 +13,14 @@ const Slide10 = ({ downIcon }) => {
   return (
     <TextAndAppletSlide
       downIcon={downIcon}
+      bg="DARK"
       title=""
       // appletSrc="./applets/triangle.htmxl"
     >
-      <Paragraph>Relation between volume of a cone and cylinder</Paragraph>
-      <Paragraph>
+      <Paragraph color={colors.WHITE}>
+        Relation between volume of a cone and cylinder
+      </Paragraph>
+      <Paragraph color={colors.WHITE}>
         Reference 1: &nbsp;
         <HyperLink
           href="https://www.geogebra.org/m/pfn6znft"
@@ -26,7 +29,7 @@ const Slide10 = ({ downIcon }) => {
           https://www.geogebra.org/m/pfn6znft
         </HyperLink>{" "}
       </Paragraph>
-      <Paragraph>
+      <Paragraph color={colors.WHITE}>
         Reference 2: &nbsp;
         <HyperLink
           href="https://www.geogebra.org/m/x54hJsVG"
