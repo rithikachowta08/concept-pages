8cc4265c0794e3fc17289970420696e27e00a86d
Abhinav Raman <abhinav.raman@byjus.com>
2022-12-28 17:38:47 +0530

    Bug fixes

--- pages/area-of-circle/slides/Slide10.js
+++ pages/area-of-circle/slides/Slide10.js
@@ -1,14 +1,14 @@
-import VideoSlide from "components/slides/VideoSlide";
-import React from "react";
+const VideoSlide = dynamic(() => import("components/slides/VideoSlide"));
+import dynamic from "next/dynamic";
 
 // video slide
 const Slide10 = ({ navBar, downIcon }) => {
    return (
       <VideoSlide
-         downIcon={downIcon}
          navBar={navBar}
          title="Area of a Circle"
          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+         isLastSlide={true}
       />
    );
 };
