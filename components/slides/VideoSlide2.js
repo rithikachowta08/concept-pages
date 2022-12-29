0b0f1b9f698b1334cd0185df41c4481764eb5f8b
arjunramesan01 <arjun.ramesan@byjus.com>
2022-12-29 14:02:02 +0530

    changed video

--- components/slides/VideoSlide2.js
+++ components/slides/VideoSlide2.js
@@ -33,14 +33,14 @@ const VideoSlide = ({ title, downIcon, isMobile }) => {
       <ShakaPlayerContainer
         videoContent={{
           dash_Url:
-            "https://qna-streaming.tllms.com/mpkgr-production-69a14fca/rac0fh/Search_QnA/220414_3/21QNANCERT08PHY01P01_Force_And_Pressure/21QNANCERT08PHY01Q04/dash/h264.mpd",
+            "https://byjus-in.akamaized.net/production/mpkgr-production-f0130697/r3wasn/Maths_INTL/211208/18INTL06MAT10/18INTL06MAT10KT012_perfect/dash/h264.mpd",
           duration: "0:05:45",
           grade: "Grade 08",
           hls_Url:
-            "https://qna-streaming.tllms.com/mpkgr-production-69a14fca/rac0fh/Search_QnA/220414_3/21QNANCERT08PHY01P01_Force_And_Pressure/21QNANCERT08PHY01Q04/hls/h264.m3u8",
+            "https://byjus-in.akamaized.net/production/mpkgr-production-f0130697/r3wasn/Maths_INTL/211208/18INTL06MAT10/18INTL06MAT10KT012_perfect/hls/h264.m3u8",
           rating: 0,
           thumbnail:
-            "https://s3.ap-south-1.amazonaws.com/byjus-media-delivery/videos/mpkgr-production-69a14fca/rac0fh/Search_QnA/220414_3/21QNANCERT08PHY01P01_Force_And_Pressure/21QNANCERT08PHY01Q04/thumbs/5b40ad1f/480x360.jpg",
+            "https://df0b18phdhzpx.cloudfront.net/video_thumbnails/production/original/792471.jpg?1671000412",
           total_video_view_count: 65,
           video_packager_id: "66869",
           video_title: "NCERT - Grade 08 - Physics - Force And Pressure - Q4",
