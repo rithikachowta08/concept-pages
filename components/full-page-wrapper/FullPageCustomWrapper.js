fca29ffadf017e74607d8c45b45755da4bffe0c5
jeeva.thangavel <jeeva.thangavel@byjus.com>
2022-12-30 09:23:24 +0530

    fix for scroll on safari

--- components/full-page-wrapper/FullPageCustomWrapper.js
+++ components/full-page-wrapper/FullPageCustomWrapper.js
@@ -8,73 +8,77 @@ import { colors } from "utils/colors";
 let fullPage;
 
 export const FullPageCustomWrapper = ({
-   slidesComponentList,
-   darkBgIndices,
-   navigationSections,
+  slidesComponentList,
+  darkBgIndices,
+  navigationSections,
 }) => {
-   const [currentPageIdx, setCurrentPageIdx] = useState(0);
-   return (
-      <>
-         <NavigationBar
-            opacity={currentPageIdx === 0 ? 0 : 1}
-            sections={navigationSections}
-            darkTheme={darkBgIndices.includes(currentPageIdx)}
-            moveTo={fullPage?.moveTo}
-            currentPageIdx={currentPageIdx}
-         />
-         <ReactFullpage
-            //fullpage options
-            // licenseKey={"YOUR_KEY_HERE"}
-            navigationTooltips={[]}
-            scrollBar={false}
-            scrollingSpeed={900}
-            fitToSectionDelay={900}
-            onLeave={function (origin, destination) {
-               setCurrentPageIdx(destination.index);
-            }}
-            render={({ state, fullpageApi }) => {
-               const moveToSection = fullpageApi?.moveTo;
-               fullPage = fullpageApi;
-               return (
-                  <>
-                     <ReactFullpage.Wrapper>
-                        {slidesComponentList.map((itm, idx) => {
-                           const downIcon = (
-                              <DownArrowIcon
-                                 visibility={
-                                    idx === slidesComponentList.length - 1
-                                       ? "hidden"
-                                       : "visible"
-                                 }
-                                 onClick={fullpageApi?.moveSectionDown}
-                                 color={
-                                    darkBgIndices.includes(idx)
-                                       ? colors.WHITE
-                                       : colors.DARK_GREY
-                                 }
-                              />
-                           );
-                           return (
-                              <div className="section" key={idx}>
-                                 {/* No navigation bar on title page */}
-                                 {React.cloneElement(itm, {
-                                    downIcon,
-                                    moveToSection,
-                                 })}
-                              </div>
-                           );
-                        })}
-                     </ReactFullpage.Wrapper>
-                  </>
-               );
-            }}
-         />
-      </>
-   );
+  const [currentPageIdx, setCurrentPageIdx] = useState(0);
+  return (
+    <>
+      <NavigationBar
+        opacity={currentPageIdx === 0 ? 0 : 1}
+        sections={navigationSections}
+        darkTheme={darkBgIndices.includes(currentPageIdx)}
+        moveTo={fullPage?.moveTo}
+        currentPageIdx={currentPageIdx}
+      />
+      <ReactFullpage
+        //fullpage options
+        // licenseKey={"YOUR_KEY_HERE"}
+        navigationTooltips={[]}
+        scrollBar={false}
+        scrollingSpeed={900}
+        fitToSectionDelay={900}
+        slidesNavigation={true}
+        slidesNavPosition={"bottom"}
+        slideSelector={".section"}
+        observer={true}
+        onLeave={function (origin, destination) {
+          setCurrentPageIdx(destination.index);
+        }}
+        render={({ state, fullpageApi }) => {
+          const moveToSection = fullpageApi?.moveTo;
+          fullPage = fullpageApi;
+          return (
+            <>
+              <ReactFullpage.Wrapper>
+                {slidesComponentList.map((itm, idx) => {
+                  const downIcon = (
+                    <DownArrowIcon
+                      visibility={
+                        idx === slidesComponentList.length - 1
+                          ? "hidden"
+                          : "visible"
+                      }
+                      onClick={fullpageApi?.moveSectionDown}
+                      color={
+                        darkBgIndices.includes(idx)
+                          ? colors.WHITE
+                          : colors.DARK_GREY
+                      }
+                    />
+                  );
+                  return (
+                    <div className="section" key={idx}>
+                      {/* No navigation bar on title page */}
+                      {React.cloneElement(itm, {
+                        downIcon,
+                        moveToSection,
+                      })}
+                    </div>
+                  );
+                })}
+              </ReactFullpage.Wrapper>
+            </>
+          );
+        }}
+      />
+    </>
+  );
 };
 
 FullPageCustomWrapper.propTypes = {
-   slidesComponentList: PropTypes.array.isRequired,
-   darkBgIndices: PropTypes.array.isRequired,
-   navigationSections: PropTypes.array.isRequired,
+  slidesComponentList: PropTypes.array.isRequired,
+  darkBgIndices: PropTypes.array.isRequired,
+  navigationSections: PropTypes.array.isRequired,
 };
