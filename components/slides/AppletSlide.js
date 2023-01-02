286e76d1177172242041a127d55fb59d566e8c1d
Rithika Chowta <rithika.chowta08@gmail.com>
2023-01-02 20:01:14 +0530

    Fix applet gap issue

--- components/slides/AppletSlide.js
+++ components/slides/AppletSlide.js
@@ -7,39 +7,39 @@ import MobileComponent from "components/layout/MobileComponent";
 import DesktopComponent from "components/layout/DesktopComponent";
 
 const IFrame = styled.iframe`
-   width: 650px;
+   aspect-ratio: 1/1;
    height: 650px;
    border-radius: 20px;
 
    // Mobile
    @media (min-width: 200px) and (max-width: 500px) and (min-height: 500px) {
       margin: auto 0;
-      width: 100vw;
       height: 100vw;
    }
 
    // Tablet
    @media (min-width: 501px) and (max-width: 992px) {
-      width: 500px;
-      height: 500px;
+      height: 100%;
+      max-width: 500px;
+      max-height: 500px;
    }
 
    // Low res desktop
    @media (min-width: 993px) and (max-width: 1224px) {
-      width: 600px;
-      height: 600px;
+      max-height: 600px;
+      height: 100%;
    }
 
    // Mobile landscape mode
    @media (min-height: 300px) and (max-height: 450px) and (max-width: 950px) {
-      width: 100vh;
       height: 100vh;
    }
 
    // Small height desktop
    @media (min-height: 400px) and (max-height: 800px) and (min-width: 900px) {
-      width: 500px;
-      height: 500px;
+      max-width: 500px;
+      max-height: 500px;
+      height: 100%;
    }
 `;
 
