75faa3b7d1de493e4fa7c6d44fc0fd1e50bf9159
Abhinav Raman <abhinav.raman@byjus.com>
2023-01-03 16:10:18 +0530

    Fixed

--- pages/surface-area-of-rectangular-prism/index.js
+++ pages/surface-area-of-rectangular-prism/index.js
@@ -26,8 +26,6 @@ const SurfaceAreaOfRectangularPrism = () => {
       },
       { title: "Total surface area", slides: [5] },
       { title: "Lateral surface area", slides: [6] },
-      { title: "Total surface area", slides: [5] },
-      { title: "Lateral surface area", slides: [6] },
    ];
 
    const darkBgIndices = [0, 2, 5, 6];
