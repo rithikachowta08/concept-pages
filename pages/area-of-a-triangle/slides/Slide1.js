a697b2a835cf3ce92d3891654e2779522e8efca7
Abhinav Raman <abhinav.raman@byjus.com>
2023-01-06 14:48:50 +0530

    Added LastPageSlide and fixed navigation

--- pages/area-of-a-triangle/slides/Slide1.js
+++ pages/area-of-a-triangle/slides/Slide1.js
@@ -8,7 +8,7 @@ const Slide1 = ({ moveToSection, downIcon }) => {
    return (
       <TitleSlide
          title={"Area of a Triangle"}
-         anchorIdxes={[2, 3, 8, 9, 18]}
+         anchorIdxes={[2, 3, 7, 8, 16]}
          contentListItems={[
             "Definition",
             "General formula",
