b50db3de94f5fbded69f5081ea099d1676ff8f60
arjunramesan01 <arjun.ramesan@byjus.com>
2023-01-09 18:50:15 +0530

    added fabicon

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,32 @@ export default class MyDocument extends Document {
           <link rel="preload" as="image" href="assets/white_bg.webp" />
           <link rel="preload" as="image" href="assets/purple_bg.webp" />
           <meta name="robots" content="noindex" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={
+              "https://search-static.byjusweb.com/assets" +
+              "/favicon/apple-touch-icon.png"
+            }
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href={
+              "https://search-static.byjusweb.com/assets" +
+              "/favicon/favicon-32x32.png"
+            }
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href={
+              "https://search-static.byjusweb.com/assets" +
+              "/favicon/favicon-16x16.png"
+            }
+          />
         </Head>
         <body>
           {/* <img src={lightBg} style={{ display: "none" }} />
