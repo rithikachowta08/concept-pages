1768f934d77fe6c7d3b63e6fbc6ad00d1ba67a64
arjunramesan01 <arjun.ramesan@byjus.com>
2023-01-09 18:33:08 +0530

    fixed redirection issue

--- pages/surface-area-of-rectangular-prism/index.js
+++ pages/surface-area-of-rectangular-prism/index.js
@@ -24,7 +24,7 @@ const SurfaceAreaOfRectangularPrism = () => {
     <PageLastSlide
       key={7}
       currentPageTitle={"Surface Area of Rectangular Prism"}
-      nextPageLink={"/study/surface-area-of-cone"}
+      nextPageLink={"/us/math/study/surface-area-of-cone"}
       nextPageTitle={"Surface Area of Cone"}
     />,
   ];
