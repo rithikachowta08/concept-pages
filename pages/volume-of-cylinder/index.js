3948f30d397678a60c1c5dcf7c1a460d58a13491
Aman Pawar <aman.pawar@toppr.com>
2023-01-09 15:43:50 +0530

    added intial slide

---
+++ pages/volume-of-cylinder/index.js
@@ -0,0 +1,55 @@
+import dynamic from "next/dynamic";
+const FullPageCustomWrapper = dynamic(() =>
+   import("components/full-page-wrapper/FullPageCustomWrapper").then(
+      (mod) => mod.FullPageCustomWrapper
+   )
+);
+const Slide1 = dynamic(() => import("./Slide1"));
+// const Slide2 = dynamic(() => import("./Slide2"));
+// const Slide3 = dynamic(() => import("./Slide3"));
+// const Slide4 = dynamic(() => import("./Slide4"));
+// const Slide5 = dynamic(() => import("./Slide5"));
+// const Slide6 = dynamic(() => import("./Slide6"));
+// const Slide7 = dynamic(() => import("./Slide7"));
+// const Slide8 = dynamic(() => import("./Slide8"));
+// const Slide9 = dynamic(() => import("./Slide9"));
+
+const VolumeOfCylinder = () => {
+   const SlideArray = [
+      <Slide1 key={0} />,
+      // <Slide2 key={1} />,
+      // <Slide3 key={2} />,
+      // <Slide4 key={3} />,
+      // <Slide5 key={4} />,
+      // <Slide6 key={5} />,
+      // <Slide7 key={6} />,
+      // <Slide8 key={7} />,
+      // <Slide9 key={8} />,
+   ];
+
+   const navigationSections = [
+      {
+         title: "What is the volume of a cylinder?",
+         slides: [1, 2, 3, 4],
+      },
+      { title: "General Formula", slides: [5] },
+      { title: "Volume of different types of cylinders", slides: [6, 7, 8] },
+   ];
+
+   const darkBgIndices = [0, 5, 6, 8];
+   return (
+      <FullPageCustomWrapper
+         slidesComponentList={SlideArray}
+         navigationSections={navigationSections}
+         darkBgIndices={darkBgIndices}
+      />
+   );
+};
+
+VolumeOfCylinder.title =
+   "Volume of a Cylinder: Definition, Formula, Examples - BYJU'S";
+
+VolumeOfCylinder.meta =
+   "Gain a deep understanding of volume of cylinder with BYJU'S. Explore the definition, formula, and examples of this concept and use our interactive activities to master it.";
+
+export default VolumeOfCylinder;
