7ef9041c67ee1fca29c2427854b0232718908f59
Rithika Chowta <rithika.chowta08@gmail.com>
2023-01-10 15:24:25 +0530

    Add first 4 slides

---
+++ pages/units-to-measure-capacity/Slide1.js
@@ -0,0 +1,23 @@
+import dynamic from "next/dynamic";
+const TitleSlide = dynamic(() => import("components/slides/TitleSlide"));
+
+const Slide1 = ({ moveToSection, downIcon }) => {
+   return (
+      <TitleSlide
+         title="Units of Capacity"
+         contentListItems={[
+            "What is capacity measurement?",
+            "Explore capacity using non-standard units",
+            "Standard units",
+            "Customary measurement system",
+            "International system of units",
+            "Imperial system of units",
+         ]}
+         anchorIdxes={[2, 7, 9]}
+         moveToSection={moveToSection}
+         downIcon={downIcon}
+      ></TitleSlide>
+   );
+};
+
+export default Slide1;
