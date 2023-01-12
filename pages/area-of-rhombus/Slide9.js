43550f4d9cf5ddc99a5b810cc5ab42fc82fa91c3
jeeva.thangavel <jeeva.thangavel@byjus.com>
2023-01-12 11:25:22 +0530

    removing extra space

--- pages/area-of-rhombus/Slide9.js
+++ pages/area-of-rhombus/Slide9.js
@@ -30,8 +30,8 @@ const Slide11 = ({ downIcon }) => {
   };
 
   let latex = [];
-  latex.push(`\\scriptscriptstyle{1}`);
-  latex.push(`\\scriptscriptstyle{2}`);
+  latex.push(`\\space\\scriptscriptstyle{1}\\space`);
+  latex.push(`\\space\\scriptscriptstyle{2}\\space`);
   latex.push(
     `= \\dfrac{1}{2} \\times ({d\\scriptscriptstyle 1} \\times {d\\scriptscriptstyle 2})`
   );
