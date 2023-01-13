dacccf651d7ef2fc33b9f386cb4ac0621571cd9f
Rithika Chowta <rithika.chowta08@gmail.com>
2023-01-13 22:57:41 +0530

    LCP test 3

--- pages/_document.js
+++ pages/_document.js
@@ -11,23 +11,9 @@ export default class MyDocument extends Document {
             <Head>
                <link
                   rel="preload"
-                  href="fonts/Nunito-Bold.ttf"
+                  href="fonts/Nunito-Bold.woff2"
                   as="font"
-                  type="font/ttf"
-                  crossOrigin=""
-               />
-               <link
-                  rel="preload"
-                  href="fonts/Nunito-Medium.ttf"
-                  as="font"
-                  type="font/ttf"
-                  crossOrigin=""
-               />
-               <link
-                  rel="preload"
-                  href="fonts/Nunito-Regular.ttf"
-                  as="font"
-                  type="font/ttf"
+                  type="font/woff2"
                   crossOrigin=""
                />
                <link rel="preload" as="image" href="assets/white_bg.webp" />
