d12f23072304bc0ebde1caf203abe800d8a795b0
Rithika Chowta <rithika.chowta08@gmail.com>
2023-01-19 10:03:45 +0530

    Readability changes 1

--- components/MathElement/EquationTable.js
+++ components/MathElement/EquationTable.js
@@ -5,13 +5,11 @@ import TeX from "@matejmazur/react-katex";
 
 const EquationParentDiv = styled.div`
    font-family: "Nunito-Medium";
+   margin-left: -5px;
 `;
 
 const StyledTable = styled.table`
-   border-spacing: 0 10px;
-   @media (min-width: 200px) and (max-width: 820px) {
-      border-spacing: 0 5px;
-   }
+   border-spacing: 5px;
 `;
 const LHSLatex = styled.td`
    text-align: right;
