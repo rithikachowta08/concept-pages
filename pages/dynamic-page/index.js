e8badd09bba12242475e749fcdcf8e587541bae8
Rithika Chowta <rithika.chowta08@gmail.com>
2023-01-27 14:07:02 +0530

    Add common elements

--- pages/dynamic-page/index.js
+++ pages/dynamic-page/index.js
@@ -1,8 +1,12 @@
 /* Common elements:
-EquationTable
-Modal
-ModalTriggerText
-TextSpanBg
-Title 
-SecondaryTitle
+Paragraph/line switch - Rithika
+EquationTable - Rithika
+Modal - Rithika
+ModalTriggerText - Aman
+ModalTriggerText with Math - Aman
+TextSpanBg - Aman
+Textspanbg with math - Aman
+Pill - Rithika
+Title - Rithika
+SecondaryTitle - Rithika
 */
