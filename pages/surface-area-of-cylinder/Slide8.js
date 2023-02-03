9274c9ce8547e7923c63b1d198be102413eb7ef9
Rithika Chowta <rithika.chowta08@gmail.com>
2023-02-03 14:12:30 +0530

    Fix build error

--- pages/surface-area-of-cylinder/Slide8.js
+++ pages/surface-area-of-cylinder/Slide8.js
@@ -4,24 +4,28 @@ const VideoSlide2 = dynamic(() => import("components/slides/VideoSlide2"), {
 });
 
 const Slide8 = ({ downIcon, currentPageIdx }) => {
-    <div style={{ height: "100%" }}>
-      <VideoSlide2
-        downIcon={downIcon}
-        videoContent={{
-          videoId: "397553",
-          title: "Derivation of formula of surface area of cylinder ",
-          dash_Url:
-            "https://s3.ap-south-1.amazonaws.com/byjus-media-delivery/videos/mpkgr-production-8da287af/robhdf/INTL_Maths/230111/SEO18INTL08MAT11KT013/dash/h264.mpd",
-          hls_Url:
-            "https://s3.ap-south-1.amazonaws.com/byjus-media-delivery/videos/mpkgr-production-8da287af/robhdf/INTL_Maths/230111/SEO18INTL08MAT11KT013/hls/h264.m3u8",
-          thumbnail:
-            "https://drive.google.com/file/d/1YGxUujGbow37QroB__cDX0nwPUmgjF0t/view?usp=share_link",
-        }}
-        downIconId="s5-video-down-icon"
-        videoSlideId="s5-video-slide"
-        currentPageIdx={currentPageIdx}
-        index={7}
-      />
-    </div>
-  );
+   return (
+      <div style={{ height: "100%" }}>
+         <VideoSlide2
+            downIcon={downIcon}
+            videoContent={{
+               videoId: "397553",
+               title: "Derivation of formula of surface area of cylinder",
+               duration: 173,
+               dash_Url:
+                  "https://s3.ap-south-1.amazonaws.com/byjus-media-delivery/videos/mpkgr-production-8da287af/robhdf/INTL_Maths/230111/SEO18INTL08MAT11KT013/dash/h264.mpd",
+               hls_Url:
+                  "https://s3.ap-south-1.amazonaws.com/byjus-media-delivery/videos/mpkgr-production-8da287af/robhdf/INTL_Maths/230111/SEO18INTL08MAT11KT013/hls/h264.m3u8",
+               thumbnail:
+                  "https://drive.google.com/file/d/1YGxUujGbow37QroB__cDX0nwPUmgjF0t/view?usp=share_link",
+            }}
+            downIconId="s5-video-down-icon"
+            videoSlideId="s5-video-slide"
+            currentPageIdx={currentPageIdx}
+            index={7}
+         />
+      </div>
+   );
 };
+
+export default Slide8;
