84d38e01e9f96ad7dce0d682f84dc0dc58464d05
Rithika Chowta <rithika.chowta08@gmail.com>
2023-02-20 12:45:51 +0530

    Add redirect for old url

--- next.config.js
+++ next.config.js
@@ -1,21 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: "/us/math/study/concept",
-  images: {
-    path: "/us/math/study/concept/_next/image/",
-    domains: [
-      "search-static.byjusweb.com",
-      "static.tllms.com",
-      "s3.ap-south-1.amazonaws.com",
-      "s3-us-west-2.amazonaws.com",
-      "infinitestudent-migration-images.s3-us-west-2.amazonaws.com",
-      "*.s3-us-west-2.amazonaws.com",
-      "cdn1.byjus.com",
-      "search-app.byjusweb.com",
-      "search-static-stg.byjusweb.com",
-      "df0b18phdhzpx.cloudfront.net",
-    ],
+   reactStrictMode: true,
+   basePath: "/us/math/study/concept",
+   images: {
+      path: "/us/math/study/concept/_next/image/",
+      domains: [
+         "search-static.byjusweb.com",
+         "static.tllms.com",
+         "s3.ap-south-1.amazonaws.com",
+         "s3-us-west-2.amazonaws.com",
+         "infinitestudent-migration-images.s3-us-west-2.amazonaws.com",
+         "*.s3-us-west-2.amazonaws.com",
+         "cdn1.byjus.com",
+         "search-app.byjusweb.com",
+         "search-static-stg.byjusweb.com",
+         "df0b18phdhzpx.cloudfront.net",
+      ],
+   },
+   async redirects() {
+    return [
+      {
+        source: '/us/math/study/:slug',
+        destination: '/us/math/study/concept/:slug', 
+        permanent: true,
+        basePath: false
+      },
+    ]
   },
 };
 
