088db953f74a3aa8e70c84ee87bcdf48a4914635
Abhinav Raman <abhinav.raman@byjus.com>
2023-02-23 17:28:12 +0530

    Minor fixes

--- components/text/BulletPoint.js
+++ components/text/BulletPoint.js
@@ -1,13 +1,16 @@
 import { Flex } from "components/StyledElements";
+import Paragraph from "./Paragraph";
 import TextSpan from "./TextSpan";
 
 const BulletPointItem = ({ children }) => {
    return (
-      <Flex>
-         <TextSpan margin="0 16px 0 0">&bull;</TextSpan>
-         {/* <span style={{ marginRight: "4px" }}>&bull;</span> */}
-         <TextSpan>{children}</TextSpan>
-      </Flex>
+      <Paragraph>
+         <Flex>
+            <TextSpan margin="0 16px 0 0">&bull;</TextSpan>
+            {/* <span style={{ marginRight: "4px" }}>&bull;</span> */}
+            <TextSpan>{children}</TextSpan>
+         </Flex>
+      </Paragraph>
    );
 };
 
