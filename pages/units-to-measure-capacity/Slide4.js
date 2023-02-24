08964d08773cecaa257b81fd351a3904e88a13b1
Rithika Chowta <rithika.chowta08@gmail.com>
2023-02-24 13:08:46 +0530

    Update package version, uncomment applet

--- pages/units-to-measure-capacity/Slide4.js
+++ pages/units-to-measure-capacity/Slide4.js
@@ -1,7 +1,7 @@
 import { coco_applet_uri } from "utils/constants";
 import dynamic from "next/dynamic";
 const AppletSlide = dynamic(() => import("components/slides/AppletSlide"));
-// import { Applet02401Ge } from "@assessed/byjus-us-math-applets/024_01_GE";
+import { Applet02401Ge } from "@assessed/byjus-us-math-applets/024_01_GE";
 
 const Slide4 = ({ downIcon, currentPageIdx }) => {
    return (
@@ -10,7 +10,7 @@ const Slide4 = ({ downIcon, currentPageIdx }) => {
          downIcon={downIcon}
          title="Explore Capacity using Non-Standard Units"
          currentPageIdx={currentPageIdx}
-         // AppletComponent={Applet02401Ge}
+         AppletComponent={Applet02401Ge}
          // appletSrc={`${coco_applet_uri}surface-area-of-cone-derivation-v4/index.html`}
       ></AppletSlide>
    );
