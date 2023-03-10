f6102d85c5231ee8cabc1c8bbfe0fcdf45945f21
Rithika Chowta <rithika.chowta08@gmail.com>
2023-03-10 11:55:09 +0530

    Add units of time page

---
+++ pages/units-of-time/Slide12.js
@@ -0,0 +1,30 @@
+import dynamic from "next/dynamic";
+const VideoSlide2 = dynamic(() => import("components/slides/VideoSlide2"), {
+   ssr: false,
+});
+
+const Slide12 = ({ downIcon, currentPageIdx }) => {
+   return (
+      <div style={{ height: "100%" }}>
+         <VideoSlide2
+            videoContent={{
+               dash_Url:
+                  "https://s3.ap-south-1.amazonaws.com/byjus-media-delivery/videos/mpkgr-production-7587de9a/rom3r9/INTL_Maths/230116/SEOINTL04MAT16KT007/dash/h264.mpd",
+               hls_Url:
+                  "https://s3.ap-south-1.amazonaws.com/byjus-media-delivery/videos/mpkgr-production-7587de9a/rom3r9/INTL_Maths/230116/SEOINTL04MAT16KT007/hls/h264.m3u8",
+               thumbnail:
+                  "https://search-mathstatic.byjusweb.com/assets/video-thumbnails/Understanding+Capacity.png",
+               duration: 129,
+               title: "Conversion of units of time",
+            }}
+            downIcon={downIcon}
+            currentPageIdx={currentPageIdx}
+            index={11}
+            downIconId="s12-video-down-icon"
+            videoSlideId="s12-video-slide"
+         />
+      </div>
+   );
+};
+
+export default Slide12;
---
+++ pages/units-of-time/Slide8.js
@@ -0,0 +1,45 @@
+import dynamic from "next/dynamic";
+const Paragraph = dynamic(() =>
+   import("components/text").then((mod) => mod.Paragraph)
+);
+const TextAndDiagramSlide = dynamic(() =>
+   import("components/slides/TextAndDiagramSlide")
+);
+const StyledImg = dynamic(() =>
+   import("components/StyledElements").then((mod) => mod.StyledImg)
+);
+
+const s8_a = "assets/units-of-time/8_a.svg";
+
+const Slide4 = ({ downIcon, currentPageIdx }) => {
+   return (
+      <TextAndDiagramSlide
+         downIcon={downIcon}
+         secondaryTitle="Units of Time"
+         title="What is a Hour?"
+         currentPageIdx={currentPageIdx}
+         diagram={
+            <StyledImg
+               src={s8_a}
+               alt="Image of hour’s hand sweeping an hour in a clock"
+            />
+         }
+      >
+         <Paragraph>
+            An hour is a unit of time equal to 60 minutes or 3600 seconds.
+         </Paragraph>
+
+         <Paragraph>
+            When the minute hand moves 60 steps, the hour hand completes one
+            step.
+         </Paragraph>
+
+         <Paragraph>
+            When the second hand moves 3600 steps, the hour hand completes one
+            step.
+         </Paragraph>
+      </TextAndDiagramSlide>
+   );
+};
+
+export default Slide4;
