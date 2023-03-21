78fa38105575be038aa5f70fc718b83b3a03c0e4
Abhinav Raman <110883724+abhinav-raman99@users.noreply.github.com>
2023-03-21 13:12:06 +0530

    Batch 8 abhinav (#270)
    * Added units of length page
    
    * Added units of area
    
    * Fixed images
    
    * Fixed images
    
    * Renamed iamges without spaces
    
    * Fixed images
    
    * Fixed slide types
    
    * Added video url and SEO
    
    * Added next page urls
    
    * Added modalImage scale
    
    * Removed unwanted
    
    * Resolve merge changes
    
    * Fixed bugs
    
    * Bug fixes

--- pages/units-of-area/index.js
+++ pages/units-of-area/index.js
@@ -34,15 +34,6 @@ const UnitsOfArea = () => {
       <Slide9 key={8} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
       <Slide10 key={9} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
       <Slide11 key={10} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
-      <Slide3 key={2} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
-      <Slide4 key={3} type={SLIDE_TYPES.VIDEO_ONLY} />,
-      <Slide5 key={4} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
-      <Slide6 key={5} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
-      <Slide7 key={6} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
-      <Slide8 key={7} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
-      <Slide9 key={8} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
-      <Slide10 key={9} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
-      <Slide11 key={10} type={SLIDE_TYPES.TEXT_AND_DIAGRAM} />,
       <Slide12 key={11} type={SLIDE_TYPES.APPLET_ONLY} />,
       <PageLastSlide
          key={16}
