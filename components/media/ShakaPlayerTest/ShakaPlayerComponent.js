b6332200feab7098e1e69fc981cb4e4ac0103923
Rithika Chowta <rithika.chowta08@gmail.com>
2023-04-11 17:22:00 +0530

    Video player fix

--- components/media/ShakaPlayerTest/ShakaPlayerComponent.js
+++ components/media/ShakaPlayerTest/ShakaPlayerComponent.js
@@ -214,6 +214,9 @@ const VideoPlayer = (props) => {
    };
 
    const onPlay = () => {
+      console.log("Played");
+      props.onPlay();
+
       onVideoStart({
          videoId: props.videoContent.videoId,
          timestamp: video.currentTime,
